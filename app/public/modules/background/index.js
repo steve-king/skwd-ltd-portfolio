d8e2442a8787a0c249f653870ac958bd1fc9e76e
Stephen King <mail@steveking.info>
2017-07-25 14:46:29 +0100

    Fixed typo

--- app/public/modules/background/index.js
+++ app/public/modules/background/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classNames from 'classNames';
+import classNames from 'classnames';
 import Parallax from 'vendor/parallax-js';
 
 import Image from 'modules/image';
